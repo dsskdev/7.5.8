52db109f2cacaa972a32158f2739bc6ae632fe58
dssk dev <desskdev@gmail.com>
2017-02-09 16:44:51 +0000

    codebase update

---
+++ umbraco7.58/scripts/script.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function() {
+    $('.search.form-group').click(function () {
+        if($(this).hasClass("active"))
+        {
+            $("input").animate({
+                width: '10%'
+            }, 350);
+            $(this).removeClass('active');
+            return false;
+        }
+        else
+        {
+            $(this).addClass('active');
+            $("input").animate({
+                width:'100%'
+            }, 350);
+            return false;
+        }       
+    });
+});
